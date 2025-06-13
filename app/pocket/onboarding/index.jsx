73e845cfa146e608884564afca07124a37a1aa9e
muafi <nmuafi1@gmail.com>
2025-06-13 09:57:33 +0700

    feat: onboarding page

--- app/pocket/onboarding/index.jsx
+++ app/pocket/onboarding/index.jsx
@@ -9,6 +9,8 @@ import { Progress, ProgressFilledTrack } from "@/components/ui/progress";
 import { router } from "expo-router";
 import { ArrowLeft } from "lucide-react-native";
 
+import ButtonNav from "../../../components/common/ButtonNav";
+
 import OnboardingIllustration from "@/assets/images/onboarding.png";
 
 export default function PocketOnboarding() {
@@ -16,40 +18,48 @@ export default function PocketOnboarding() {
     router.back();
   };
 
+  const handleNext = () => {
+    router.push("pocket/onboarding/CreatePocketOnboarding");
+  };
+
   return (
-    <Box className="flex-1 bg-white px-6">
-      <Box className="flex justify-start my-5">
-        <Pressable onPress={handleBack}>
-          <ArrowLeft size={24} />
-        </Pressable>
+    <Box className="flex-1 bg-white px-6 justify-between">
+      <Box className="flex flex-col">
+        <Box className="flex justify-start my-5">
+          <Pressable onPress={handleBack}>
+            <ArrowLeft size={24} />
+          </Pressable>
+        </Box>
+
+        <Center className="w-full h-16 mt-2">
+          <Progress
+            value={40}
+            size="xs"
+            orientation="horizontal"
+            className="w-full h-2"
+          >
+            <ProgressFilledTrack className="bg-[#3FD8D4] h-2" />
+          </Progress>
+        </Center>
+
+        <Image
+          size="2xl"
+          source={OnboardingIllustration}
+          className="w-full justify-center items-center mt-10 mb-10"
+          alt="onboarding-illustration"
+        />
+
+        <Heading size="xl" bold="true" className="font-extrabold">
+          Wujudkan Tujuan Bersama dengan Mudah!
+        </Heading>
+
+        <Text className="text-lg mt-2">
+          Kumpulkan dana dengan keluarga atau teman untuk impian bersama, lebih
+          terencana dan juga transparan.
+        </Text>
       </Box>
 
-      <Center className="w-full h-16 mt-2">
-        <Progress
-          value={40}
-          size="xs"
-          orientation="horizontal"
-          className="w-full h-2"
-        >
-          <ProgressFilledTrack className="bg-[#3FD8D4] h-2" />
-        </Progress>
-      </Center>
-
-      <Image
-        size="2xl"
-        source={OnboardingIllustration}
-        className="w-full justify-center items-center mt-12 mb-10"
-        alt="onboarding-illustration"
-      />
-
-      <Heading size="2xl" bold="true" className="font-extrabold">
-        Wujudkan Tujuan Bersama dengan Mudah!
-      </Heading>
-
-      <Text className="text-normal mt-5">
-        Kumpulkan dana dengan keluarga atau teman untuk impian bersama, lebih
-        terencana dan juga transparan.
-      </Text>
+      <ButtonNav onPress={handleNext} text="Buat Pocket" className="mb-8" />
     </Box>
   );
 }
