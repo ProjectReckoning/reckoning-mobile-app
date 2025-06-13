3b0255d6b590a8c2a960689924a205517100a60b
Fabrian28 <fabrianrafi@outlook.com>
2025-06-13 10:48:31 +0700

    chore: additional configs
    add react native svg & svg transformer, add assets, update metro and tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -168,6 +168,29 @@ module.exports = {
           info: "rgb(var(--color-indicator-info)/<alpha-value>)",
           error: "rgb(var(--color-indicator-error)/<alpha-value>)",
         },
+        "pink-wondr": "#FDA9FF",
+        "pink-wondr-dark": "#E190E0",
+
+        "purple-wondr": "#A471E1",
+        "purple-wondr-dark": "#8B5BB5",
+
+        "tosca-wondr": "#3FD8D4",
+        "tosca-wondr-dark": "#2EACAA",
+
+        "lime-wondr": "#DEEF5A",
+        "lime-wondr-dark": "#B5D040",
+
+        "orange-wondr": "#FF8500",
+        "orange-wondr-dark": "#E17500",
+
+        "yellow-wondr": "#FFC533",
+        "yellow-wondr-dark": "#D4A82A",
+
+        "red-wondr": "#D65C5B",
+        "red-wondr-dark": "#B24F4F",
+
+        "green-wondr": "#02C694",
+        "green-wondr-dark": "#01A37B",
       },
       fontFamily: {
         heading: undefined,
