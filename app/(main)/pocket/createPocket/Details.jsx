49534c14ed2a048d91046fb27a45470558de3e35
muafi <nmuafi1@gmail.com>
2025-06-15 01:24:14 +0700

    feat: create pocket detail

--- app/(main)/pocket/createPocket/Details.jsx
+++ app/(main)/pocket/createPocket/Details.jsx
@@ -4,16 +4,94 @@ import { Image } from "@/components/ui/image";
 import { VStack } from "@/components/ui/vstack";
 import { Heading } from "@/components/ui/heading";
 import { Pressable } from "@/components/ui/pressable";
+import { AlertCircleIcon } from "@/components/ui/icon";
+import { DatePickerModal } from "react-native-paper-dates";
+import { Input, InputField, InputSlot } from "@/components/ui/input";
+import {
+  FormControl,
+  FormControlError,
+  FormControlErrorText,
+  FormControlErrorIcon,
+  FormControlLabel,
+  FormControlLabelText,
+} from "@/components/ui/form-control";
 
 import { router } from "expo-router";
-import { ArrowLeft } from "lucide-react-native";
+import { useState, useEffect, useCallback } from "react";
+import { ArrowLeft, CalendarClock } from "lucide-react-native";
 
 import GoalDecorator from "../../../../assets/images/decorators/goal-decorator.png";
 import PrimaryButton from "../../../../components/common/buttons/PrimaryButton";
 
 export default function Details() {
+  const [inputName, setInputName] = useState("");
+  const [isNameInvalid, setNameIsInvalid] = useState(false);
+  const [inputAmount, setInputAmount] = useState("");
+  const [isAmountInvalid, setAmountIsInvalid] = useState(false);
+  const [isDateInvalid, setDateIsInvalid] = useState(false);
+  const [range, setRange] = useState({
+    startDate: undefined,
+    endDate: undefined,
+  });
+  const [open, setOpen] = useState(false);
+  const [dateTouched, setDateTouched] = useState(false);
+
+  const handleOpenDatePicker = () => {
+    setOpen(true);
+    setDateTouched(true);
+  };
+
+  const onDismiss = useCallback(() => {
+    setOpen(false);
+  }, []);
+
+  const onConfirm = useCallback(({ startDate, endDate }) => {
+    setOpen(false);
+    setRange({ startDate, endDate });
+  }, []);
+
+  const handleSubmit = () => {
+    if (!isNameInvalid && !isAmountInvalid && !isDateInvalid) {
+      console.log("OK");
+      // router.push("pocket/createPocket/SelectGoal");
+    }
+  };
+
   const handleBack = () => {
     router.back();
+  };
+
+  useEffect(() => {
+    if (inputName.length > 20 || inputName.length === null) {
+      setNameIsInvalid(true);
+    } else {
+      setNameIsInvalid(false);
+    }
+  }, [inputName]);
+
+  useEffect(() => {
+    const amountValue = parseInt(inputAmount.replace(/\D/g, ""), 10);
+    if (amountValue < 10000) {
+      setAmountIsInvalid(true);
+    } else {
+      setAmountIsInvalid(false);
+    }
+  }, [inputAmount]);
+
+  useEffect(() => {
+    if (dateTouched) {
+      if (!range.startDate || !range.endDate) {
+        setDateIsInvalid(true);
+      } else {
+        setDateIsInvalid(false);
+      }
+    }
+  }, [range, dateTouched]);
+
+  const formatCurrency = (value) => {
+    if (!value) return "";
+    const numeric = value.replace(/\D/g, "");
+    return numeric.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
   };
 
   return (
@@ -57,11 +135,139 @@ export default function Details() {
 
       <Box className="flex-1 justify-between px-6">
         <Box className="flex flex-col">
-          <Heading size="xl" className="font-bold my-8">
+          <Heading size="xl" className="font-bold mt-8 mb-5">
             Detail Pocket
           </Heading>
+
+          <VStack space="md" className="w-full">
+            <FormControl
+              isInvalid={isNameInvalid}
+              size="md"
+              isDisabled={false}
+              isReadOnly={false}
+              isRequired={true}
+            >
+              <FormControlLabel>
+                <FormControlLabelText className="font-light">
+                  Nama pocket
+                </FormControlLabelText>
+              </FormControlLabel>
+              <Input
+                className="h-14 my-1 rounded-xl border-gray-300 data-[focus=true]:border-[#007BE5]"
+                size="lg"
+              >
+                <InputField
+                  type="text"
+                  placeholder="Tulis nama pocket"
+                  value={inputName}
+                  onChangeText={(text) => setInputName(text)}
+                  className="p-3"
+                />
+                <InputSlot className="mr-3">
+                  <Text
+                    className={`text-gray-500 text-xs ${isNameInvalid && "text-red-500"}`}
+                  >
+                    {inputName.length}/20
+                  </Text>
+                </InputSlot>
+              </Input>
+              <FormControlError>
+                <FormControlErrorIcon as={AlertCircleIcon} />
+                <FormControlErrorText>
+                  Nama pocket mu harus terisi & maksimal 20 karakter!
+                </FormControlErrorText>
+              </FormControlError>
+            </FormControl>
+
+            <FormControl
+              isInvalid={isAmountInvalid}
+              size="md"
+              isDisabled={false}
+              isReadOnly={false}
+              isRequired={true}
+            >
+              <FormControlLabel>
+                <FormControlLabelText className="font-light">
+                  Tentuin target saldo kalian
+                </FormControlLabelText>
+              </FormControlLabel>
+              <Input
+                className="h-14 my-1 rounded-xl border-gray-300 data-[focus=true]:border-[#007BE5]"
+                size="lg"
+              >
+                <InputSlot className="ml-4">
+                  <Text className="text-black">Rp</Text>
+                </InputSlot>
+                <InputField
+                  type="text"
+                  placeholder="5.000.000"
+                  value={formatCurrency(inputAmount)}
+                  onChangeText={(text) => setInputAmount(text)}
+                  className="p-3 pl-0"
+                  keyboardType="numeric"
+                />
+              </Input>
+              <FormControlError>
+                <FormControlErrorIcon as={AlertCircleIcon} />
+                <FormControlErrorText>
+                  Minimal target saldo adalah Rp10.000!
+                </FormControlErrorText>
+              </FormControlError>
+            </FormControl>
+
+            <FormControl
+              isInvalid={isDateInvalid}
+              size="md"
+              isDisabled={false}
+              isReadOnly={false}
+              isRequired={true}
+            >
+              <FormControlLabel>
+                <FormControlLabelText className="font-light">
+                  Durasi target
+                </FormControlLabelText>
+              </FormControlLabel>
+              <Pressable
+                onPress={handleOpenDatePicker}
+                className="w-full h-14 p-3 my-1 justify-center rounded-xl border border-gray-300 active:border-[#007BE5] focus:border-[#007BE5]"
+              >
+                <Box className="flex flex-row gap-3 items-center">
+                  <CalendarClock size={16} className="text-gray-300" />
+                  {range.startDate && range.endDate ? (
+                    <Text className="text-black text-lg">
+                      {range.startDate.toLocaleDateString("id-ID")} -{" "}
+                      {range.endDate.toLocaleDateString("id-ID")}
+                    </Text>
+                  ) : (
+                    <Text className="text-gray-400 text-lg">Pilih tanggal</Text>
+                  )}
+
+                  <DatePickerModal
+                    locale="id"
+                    mode="range"
+                    visible={open}
+                    onDismiss={onDismiss}
+                    startDate={range.startDate}
+                    endDate={range.endDate}
+                    onConfirm={onConfirm}
+                    presentationStyle="pageSheet"
+                  />
+                </Box>
+              </Pressable>
+              <FormControlError>
+                <FormControlErrorIcon as={AlertCircleIcon} />
+                <FormControlErrorText>
+                  Target harus memiliki tanggal mulai dan selesai!
+                </FormControlErrorText>
+              </FormControlError>
+            </FormControl>
+          </VStack>
         </Box>
-        <PrimaryButton buttonTitle="Lanjut" className="mt-3 mb-8" />
+        <PrimaryButton
+          buttonAction={handleSubmit}
+          buttonTitle="Lanjut"
+          className="mt-3 mb-8"
+        />
       </Box>
     </Box>
   );
