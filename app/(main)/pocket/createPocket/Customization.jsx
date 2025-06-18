c05f8139d0708e33e96da7b2eee2d91aede1c63e
muafi <nmuafi1@gmail.com>
2025-06-18 12:46:40 +0700

    fix: fix bug refactor pocket color data

--- app/(main)/pocket/createPocket/Customization.jsx
+++ app/(main)/pocket/createPocket/Customization.jsx
@@ -20,15 +20,53 @@ import {
   resetPocketData,
   pocketValidation,
 } from "../../../../utils/pocketCustomization/pocketValidation";
+
 import {
   iconKeys,
   iconMap,
   iconWhiteMap,
 } from "../../../../utils/pocketCustomization/personalPocketIconUtils";
+
 import {
   colors,
   colorMap,
 } from "../../../../utils/pocketCustomization/pocketColorUtils";
+
+// const colors = [
+//   "bg-orange-wondr",
+//   "bg-yellow-wondr",
+//   "bg-lime-wondr",
+//   "bg-tosca-wondr",
+//   "bg-purple-wondr",
+//   "bg-pink-wondr",
+// ];
+
+// const colorMap = {
+//   "bg-orange-wondr": {
+//     solid: "bg-orange-wondr",
+//     translucent: "bg-orange-wondr-light-translucent",
+//   },
+//   "bg-yellow-wondr": {
+//     solid: "bg-yellow-wondr",
+//     translucent: "bg-yellow-wondr-light-translucent",
+//   },
+//   "bg-lime-wondr": {
+//     solid: "bg-lime-wondr",
+//     translucent: "bg-lime-wondr-light-translucent",
+//   },
+//   "bg-tosca-wondr": {
+//     solid: "bg-tosca-wondr",
+//     translucent: "bg-tosca-wondr-light-translucent",
+//   },
+//   "bg-purple-wondr": {
+//     solid: "bg-purple-wondr",
+//     translucent: "bg-purple-wondr-light-translucent",
+//   },
+//   "bg-pink-wondr": {
+//     solid: "bg-pink-wondr",
+//     translucent: "bg-pink-wondr-light-translucent",
+//   },
+// };
 
 export default function Customization() {
   const [selectedColorIndex, setSelectedColorIndex] = useState(null);
