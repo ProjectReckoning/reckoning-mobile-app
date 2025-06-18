c01c2bbc8f519920113d47817454aabb3edfaf81
muafi <nmuafi1@gmail.com>
2025-06-18 11:47:00 +0700

    fix: refactor pocket color data

--- app/(main)/pocket/createPocket/Customization.jsx
+++ app/(main)/pocket/createPocket/Customization.jsx
@@ -25,41 +25,10 @@ import {
   iconMap,
   iconWhiteMap,
 } from "../../../../utils/pocketCustomization/personalPocketIconUtils";
-
-const colors = [
-  "bg-orange-wondr",
-  "bg-yellow-wondr",
-  "bg-lime-wondr",
-  "bg-tosca-wondr",
-  "bg-purple-wondr",
-  "bg-pink-wondr",
-];
-const colorMap = {
-  "bg-orange-wondr": {
-    solid: "bg-orange-wondr",
-    translucent: "bg-orange-wondr-light-translucent",
-  },
-  "bg-yellow-wondr": {
-    solid: "bg-yellow-wondr",
-    translucent: "bg-yellow-wondr-light-translucent",
-  },
-  "bg-lime-wondr": {
-    solid: "bg-lime-wondr",
-    translucent: "bg-lime-wondr-light-translucent",
-  },
-  "bg-tosca-wondr": {
-    solid: "bg-tosca-wondr",
-    translucent: "bg-tosca-wondr-light-translucent",
-  },
-  "bg-purple-wondr": {
-    solid: "bg-purple-wondr",
-    translucent: "bg-purple-wondr-light-translucent",
-  },
-  "bg-pink-wondr": {
-    solid: "bg-pink-wondr",
-    translucent: "bg-pink-wondr-light-translucent",
-  },
-};
+import {
+  colors,
+  colorMap,
+} from "../../../../utils/pocketCustomization/pocketColorUtils";
 
 export default function Customization() {
   const [selectedColorIndex, setSelectedColorIndex] = useState(null);
