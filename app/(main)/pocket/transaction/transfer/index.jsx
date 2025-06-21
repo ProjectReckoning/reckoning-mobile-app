7c2090a41e098e47fc1435a548f97be9463ad71e
muafi <nmuafi1@gmail.com>
2025-06-21 19:50:36 +0700

    feat: shadow box and modify the search bar into pressable

--- app/(main)/pocket/transaction/transfer/index.jsx
+++ app/(main)/pocket/transaction/transfer/index.jsx
@@ -1,14 +1,16 @@
 import { Box } from "@/components/ui/box";
-import { Searchbar } from "react-native-paper";
+import { Text } from "@/components/ui/text";
 import { HStack } from "@/components/ui/hstack";
 import { Heading } from "@/components/ui/heading";
+import { Pressable } from "@/components/ui/pressable";
 
 import { useState } from "react";
+import { Search } from "lucide-react-native";
 import TabBar from "@/components/common/TabBar";
-import { WondrColors } from "@/utils/colorUtils";
 import { usePocketStore } from "@/stores/pocketStore";
 import FriendList from "@/components/common/FriendList";
 import AppBar from "../../../../../components/common/AppBar";
+import { WondrColors } from "../../../../../utils/colorUtils";
 import { transferFeatures } from "@/utils/mockData/featureData";
 import FeatureButton from "@/components/common/buttons/FeatureButton";
 
@@ -18,7 +20,6 @@ const tabList = [
 ];
 
 export default function Transfer() {
-  const [searchQuery, setSearchQuery] = useState("");
   const [activeTab, setActiveTab] = useState("tersimpan");
   const { selectedFriends, setSelectedFriends } = usePocketStore();
 
@@ -40,42 +41,27 @@ export default function Transfer() {
         <Box className="w-80 h-32 bg-gray-wondr-border rounded-2xl absolute bottom-5 -z-10"></Box>
       </Box>
 
-      <Searchbar
-        mode="bar"
-        placeholder="Mau transfer ke siapa hari ini?"
-        onChangeText={setSearchQuery}
-        value={searchQuery}
-        style={{
-          backgroundColor: "#fff",
-          borderRadius: 10,
-          borderWidth: 1,
-          borderColor: WondrColors["dark-gray-wondr-deactive"],
-          padding: 2,
-          height: 50,
-          justifyContent: "center",
-          alignItems: "center",
-          paddingLeft: 0,
-        }}
-        inputStyle={{
-          color: WondrColors["gray-wondr-border"],
-          fontSize: 16,
-          fontFamily: "Poppins",
-          textAlignVertical: "center",
-          textAlign: "left",
-          paddingVertical: 0,
-        }}
-        placeholderTextColor={WondrColors["gray-wondr-border"]}
-        iconColor={WondrColors["dark-gray-wondr-deactive"]}
-      />
+      <Pressable>
+        <HStack
+          space="lg"
+          className="w-full h-14 p-3 items-center justify-start border border-dark-gray-wondr-deactive rounded-lg active:bg-gray-50"
+        >
+          <Search size={21} color={WondrColors["dark-gray-wondr-deactive"]} />
+          <Text size="lg" className="font-light">
+            Mau transfer ke siapa hari ini?
+          </Text>
+        </HStack>
+      </Pressable>
 
       <TabBar
         tabList={tabList}
         activeTab={activeTab}
         setActiveTab={setActiveTab}
         size={16}
+        marginVertical={21}
       />
 
-      <Heading className="mb-3">6/200 Penerima</Heading>
+      <Heading className="mb-5">6/200 Penerima</Heading>
       <FriendList
         selectedFriends={selectedFriends}
         setSelectedFriends={setSelectedFriends}
