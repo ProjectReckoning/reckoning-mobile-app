0a758c78cf760afef2dfeb09589e8ad723f938f3
Laras <ivankalarasati@gmail.com>
2025-06-23 14:43:12 +0700

    feat: [TAKUA-252,253] Modify roles and remove for members modal
    Create modify roles and remove for members modal

--- components/common/tableCells/ReusableCellContent.jsx
+++ components/common/tableCells/ReusableCellContent.jsx
@@ -2,34 +2,38 @@ import React from "react";
 import { Box } from "@/components/ui/box";
 import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
+// Tidak perlu mengimpor Icon atau WondrColors di sini
 
 export default function ReusableCellContent({
   icon,
   title,
   description,
   date,
-  // --- TAMBAHKAN PROPS BARU UNTUK MENERIMA CUSTOM CLASSNAME ---
+  // Prop untuk menerima warna latar belakang ikon
+  iconContainerBgColor,
+  // Props untuk kustomisasi className
   titleClassName,
   descriptionClassName,
   dateClassName,
 }) {
-  // Gabungkan kelas dasar dengan kelas custom yang diberikan melalui props.
-  // Jika tidak ada props custom yang diberikan, maka akan menggunakan kelas dasar saja.
-  const finalTitleClass = `font-bold text-lg text-black ${titleClassName || ""}`;
-  const finalDescriptionClass = `font-base text-base text-black ${descriptionClassName || ""}`;
+  const finalTitleClass = `text-lg text-black ${titleClassName || "font-bold text-lg"}`;
+  const finalDescriptionClass = `  ${descriptionClassName || "text-black font-semibold text-base"}`;
   const finalDateClass = `font-base text-sm text-black ${dateClassName || ""}`;
+
+  // Tentukan warna latar belakang. Jika tidak ada yang diberikan, gunakan warna abu-abu sebagai default.
+  const bgColor = iconContainerBgColor || "bg-light-gray-wondr";
 
   return (
     <Box className="flex-row gap-5 items-center flex-1 pr-2">
-      <Box className="justify-center items-center rounded-full bg-light-gray-wondr w-16 h-16">
+      <Box
+        className={`justify-center items-center rounded-full w-16 h-16 ${bgColor}`}
+      >
         <Box>{icon}</Box>
       </Box>
 
       <Box className="flex-1">
-        {/* Tambahkan flex-1 agar teks tidak terpotong jika terlalu panjang */}
         <VStack space="xxs">
           <Text className={finalTitleClass}>{title}</Text>
-
           <Text
             className={finalDescriptionClass}
             numberOfLines={1}
@@ -37,7 +41,6 @@ export default function ReusableCellContent({
           >
             {description}
           </Text>
-
           {date && (
             <Text
               className={finalDateClass}
